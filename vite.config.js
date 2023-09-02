76455ac4d252119a538673cecea05a8b5271a234
felipe7 <lipebroken@gmail.com>
2023-09-01 22:14:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazine-hashtag/",
+  base: "/",
   plugins: [],
   resolve: {
     /*something*/
